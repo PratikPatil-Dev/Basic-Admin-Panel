1a0fe3a306743ecdd864bf23ed54409cefd3ba1f
foxyfoxxx <123150727+foxyfoxxx@users.noreply.github.com>
2023-05-26 13:50:38 +0530

    Update script.js - new API link added

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
   function getData() {
     return new Promise(function (resolve, reject) {
       $.get(
-        "http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D",
+        "https://admin-panel-data-edyoda-sourav.vercel.app/admin/data",
         function (userData) {
           resolve(userData).fail(function (error) {
             reject(error);
